3bfd0bd57a8d8a24a74345a00065f51e452705c8
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2024-10-21 23:49:12 -0300

    Finalizando o pop-up da pagina home do usuário

--- projeto/cardapioFordao/resources/views/home.blade.php
+++ projeto/cardapioFordao/resources/views/home.blade.php
@@ -38,30 +38,41 @@
             @endisset
 
             @forelse ($categorias as $categoria)
-                <div class="categoria" id="{{$categoria->id}}">
+                <div class="categoria" id="{{ $categoria->id }}">
                     <h2 class="tituloCategoria">{{ $categoria->nome }}</h2>
                     <div class="corpoCategoria">
-                      @forelse ($dados[$categoria->id] as $produto)
-                        <div class="card">
-                          <img src="{{asset($produto->linkImagem)}}">
-                          <div>
-                              <h3>{{$produto->nome}}</h3>
-                              <span>R$ {{ $produto->preco }}</span>
-                              <button>Ler mais</button>
-                          </div>
-                        </div>
-                      @empty
-                          
-                      @endforelse
+                        @forelse ($dados[$categoria->id] as $produto)
+                            <div class="card">
+                                <img src="{{ asset($produto->linkImagem) }}">
+                                <div>
+                                    <h3>{{ $produto->nome }}</h3>
+                                    <span>R$ {{ $produto->preco }}</span>
+                                    <button class="openPopup" onclick="showPopUp('{{ asset($produto->linkImagem) }}','{{$produto->nome}}', '{{$produto->descricao}}', '{{$produto->preco}}')">Ler mais</button>
+
+
+
+                                </div>
+                            </div>
+                        @empty
+                        @endforelse
                     </div>
                 </div>
             @empty
                 <p class="naoHa">Não há produtos disponíveis</p>
             @endforelse
+            <div id="popupBlur" class="popupBlur">
+                <div id="popup-content">
+                    <h3>Detalhes</h3>
+                    <span class="close" id="closePopup" onclick="closePopUp()">X</span>
+                    <img id="productImage"></img>
+                    <h4 id="productName">Nome do Produto</h4>
+                    <p id="productDescription">Descrição do Produto</p>
+                    <p id="productPrice">Preço: R$ 100,00</p>
+                </div>
+            </div>
     </main>
-
-
 </body>
 <script src="{{ asset('js/fontCode.js') }}"></script>
+<script src="{{ asset('js/popUp.js') }}"></script>
 
 </html>
