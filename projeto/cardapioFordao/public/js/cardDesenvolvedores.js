3097bac109dac17d4068179a07d0cddbf2ea6d72
GiovanniEliasDaRosa <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2024-11-14 07:33:10 -0300

    Arrumando uns negocios

--- projeto/cardapioFordao/public/js/cardDesenvolvedores.js
+++ projeto/cardapioFordao/public/js/cardDesenvolvedores.js
@@ -1,34 +1,29 @@
+const container = document.getElementById("cardContainer");
 
+function createCard(name, role, photo, linkedin) {
+    const card = document.createElement("div");
 
-const container = document.getElementById('cardContainer');
+    card.classList.add("card");
 
+    const cardImage = document.createElement("div");
 
-function createCard(name, role, photo, phone, linkedin) {
+    cardImage.classList.add("card-image");
 
-  const card = document.createElement('div');
+    cardImage.style.backgroundImage = `url(${photo})`;
 
-  card.classList.add('card');
+    const cardContent = document.createElement("div");
 
-  const cardImage = document.createElement('div');
+    cardContent.classList.add("card-content");
 
-  cardImage.classList.add('card-image');
-
-  cardImage.style.backgroundImage = `url(${photo})`;
-
-  const cardContent = document.createElement('div');
-
-  cardContent.classList.add('card-content');
-  
-  cardContent.innerHTML = `
+    cardContent.innerHTML = `
     <h2>${name}</h2>
     <p>${role}</p>
-    <p>Telefone: ${phone}</p>
     <a href="${linkedin}" target="_blank">LinkedIn</a>
   `;
 
-  card.appendChild(cardImage);
-  card.appendChild(cardContent);
-  container.appendChild(card);
+    card.appendChild(cardImage);
+    card.appendChild(cardContent);
+    container.appendChild(card);
 }
 
 // Exemplo de como criar cards
--- projeto/cardapioFordao/resources/views/desenvolvedores.blade.php
+++ projeto/cardapioFordao/resources/views/desenvolvedores.blade.php
@@ -2,33 +2,41 @@
 <html>
 
 <head>
-    <title>Quem somos nós?</title>
-    <link rel="stylesheet" href="{{ asset('css/desenvolvedores.css') }}">
+  <title>Quem somos nós?</title>
+  <link rel="stylesheet" href="{{ asset('css/desenvolvedores.css') }}">
 </head>
 
 <body>
-    <header>
-        <h1>Desenvolvedores</h1>
-    </header>
-    <main>
-        <div id="conteudo">
-            <p>O presente Trabalho de Conclusão de Curso (TCC) foi elaborado no âmbito do curso técnico em Informática para Internet, oferecido pela Escola Técnica Estadual (ETEC) João Belarmino, em parceria com o Centro Paula Souza, e desenvolvido nas dependências da Escola Estadual Professor Clodoveu Barbosa. Sob a orientação do professor Rubens Castaldelli, a equipe, composta por João, Miguel, Tiago, Geovana e Rebeca, dedicou-se ao elaborar o projeto. As responsabilidades foram distribuídas da seguinte forma: João, Miguel e Tiago atuaram no desenvolvimento backend, responsáveis pela construção da lógica e infraestrutura do sistema; Geovana ficou responsável pelo desenvolvimento frontend, e concebeu a interface gráfica do usuário, enquanto Rebeca, se encarregou da documentação do projeto, se responsabilizando pelo registro de todo o processo dessa produção, desde a concepção inicial até a implementação final.</p>
-        </div>
-        <div id="cardContainer"></div>
-    </main>
-    <script src="{{ asset('js/cardDesenvolvedores.js') }}"></script>
-    <script>
-        createCard('Geovana Noronha Rodrigues', 'Desenvolvedora Front-end', "{{ asset('pictures/noImage.jpg') }}", '19 99766-5087',
-            'https://www.linkedin.com/in/');
-        createCard('João Felipe Lopes dos Santos', 'Desenvolvedor Back-end', "{{ asset('pictures/noImage.jpg') }}", '19 99897-5197',
-            'https://www.linkedin.com/in/joao-silva');
-        createCard('Miguel Granato Padilha', 'Desenvolvedor Back-end', "{{ asset('pictures/noImage.jpg') }}", '19 97150-1962',
-            'https://www.linkedin.com/in/');
-        createCard('Rebeca Civera Marchi', 'Documentação', "{{ asset('pictures/noImage.jpg') }}", '19 99245-7692',
-            'https://www.linkedin.com/in/');
-        createCard('Tiago Citrangulo da Silva', 'Desenvolvedor Full-stack', "{{ asset('pictures/noImage.jpg') }}", '19 97159-5502',
-            'https://www.linkedin.com/in/TiagoCitranguloDaSilva/');
-    </script>
+  <header>
+    <h1>Desenvolvedores</h1>
+  </header>
+  <main>
+    <div id="conteudo">
+      <p>O presente Trabalho de Conclusão de Curso (TCC) foi elaborado no âmbito do curso técnico em Informática para
+        Internet, oferecido pela Escola Técnica Estadual (ETEC) João Belarmino, em parceria com o Centro Paula Souza, e
+        desenvolvido nas dependências da Escola Estadual Professor Clodoveu Barbosa. Sob a orientação do professor
+        Rubens Castaldelli, a equipe, composta por João, Miguel, Tiago, Geovana e Rebeca, dedicou-se ao elaborar o
+        projeto. As responsabilidades foram distribuídas da seguinte forma: João, Miguel e Tiago atuaram no
+        desenvolvimento backend, responsáveis pela construção da lógica e infraestrutura do sistema; Geovana ficou
+        responsável pelo desenvolvimento frontend, e concebeu a interface gráfica do usuário, enquanto Rebeca, se
+        encarregou da documentação do projeto, se responsabilizando pelo registro de todo o processo dessa produção,
+        desde a concepção inicial até a implementação final.</p>
+    </div>
+    <div id="cardContainer"></div>
+  </main>
+  <script src="{{ asset('js/cardDesenvolvedores.js') }}"></script>
+  <script>
+    createCard('Geovana Noronha Rodrigues', 'Desenvolvedora Front-end', "{{ asset('pictures/noImage.jpg') }}",
+      'https://www.linkedin.com/in/');
+    createCard('João Felipe Lopes dos Santos', 'Desenvolvedor Back-end', "{{ asset('pictures/noImage.jpg') }}",
+      'https://www.linkedin.com/in/joao-silva');
+    createCard('Miguel Granato Padilha', 'Desenvolvedor Back-end', "{{ asset('pictures/noImage.jpg') }}",
+      'https://www.linkedin.com/in/');
+    createCard('Rebeca Civera Marchi', 'Documentação', "{{ asset('pictures/noImage.jpg') }}",
+      'https://www.linkedin.com/in/');
+    createCard('Tiago Citrangulo da Silva', 'Desenvolvedor Full-stack', "{{ asset('pictures/noImage.jpg') }}",
+      'https://www.linkedin.com/in/TiagoCitranguloDaSilva/');
+  </script>
 </body>
 
 </html>
